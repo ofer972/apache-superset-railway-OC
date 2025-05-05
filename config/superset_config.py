04ecb9efa8e2631fa407fbad84919ea20b8e9cd8
ofer972 <ofer972@gmail.com>
2025-05-05 23:35:05 +0300

    Update superset_config.py

--- config/superset_config.py
+++ config/superset_config.py
@@ -10,6 +10,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE")
 
+HTML_SANITIZATION = False
+
 ###AUTH_ROLE_PUBLIC = 'Public'
 ###PUBLIC_ROLE_LIKE = 'Gamma'
 ###PUBLIC_ROLE_LIKE_GAMMA = True
